7057f1501a1dbb0129a13043f89aafe3fa04a040
TomZ28 <tommyzhang2809@gmail.com>
2025-05-07 22:20:45 -0400

    Add loading Suspense wrapper to application for compiling purposes (fix Next error) + Add missing applications not-found page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import Navbar from '@/app/ui/dashboard/navbar';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -6,7 +8,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <Navbar />
       </div>
-      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">
+        <Suspense fallback={<Loading />}>
+          {children}
+        </Suspense>
+      </div>
     </div>
   );
 }
